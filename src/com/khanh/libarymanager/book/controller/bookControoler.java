22a37e88332fdad56f66bfd23b10aa173b3a6148
Tkanh12-10-2024 <letakhanh131@gmail.com>
2026-07-25 22:46:15 +0700

    book controllrt update 25/7

---
+++ src/com/khanh/libarymanager/book/controller/bookControoler.java
@@ -0,0 +1,10 @@
+package com.khanh.libarymanager.book.controller;
+
+public class bookControoler {
+    public String name;
+    public long id;
+    public String ISBN; // ma sach quoc te
+    public float price;
+
+
+}
