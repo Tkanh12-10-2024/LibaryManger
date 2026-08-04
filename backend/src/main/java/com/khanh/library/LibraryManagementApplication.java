0bbc54a47b0cfa4a77d8a68df491a5e7bcd1d292
Tkanh12-10-2024 <letakhanh131@gmail.com>
2026-08-04 23:33:38 +0700

    catergoty

--- backend/src/main/java/com/khanh/library/LibraryManagementApplication.java
+++ backend/src/main/java/com/khanh/library/LibraryManagementApplication.java
@@ -1,4 +1,13 @@
-package com.khanh.libarymanager;
+package main.java.com.khanh.library;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementApplication.class, args);
+    }
+
 }
--- backend/src/main/java/com/khanh/library/Main.java
+++
@@ -1,7 +0,0 @@
-package com.khanh.libarymanager;
-
-public class Main {
-    public static  void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
